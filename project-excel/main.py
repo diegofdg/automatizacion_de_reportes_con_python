4292961468c64881a5e8f21b32db6dcd687bb2dd
diegofdg <diegofdg@gmail.com>
2025-06-02 19:22:50 -0300

    lectura de datos de excel

---
+++ project-excel/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+PATH_CARPETA = r'D:\mis_proyectos\ciencia_de_datos\automatizacion_de_reportes_con_python\project-excel\inputs'
+
+NOTAS_ALUMNOS_PATH = PATH_CARPETA + r'\Notas_Alumnos.xlsx'
+
+def main():
+    excel_df = pd.read_excel(NOTAS_ALUMNOS_PATH, sheet_name='Notas')
+    for index, row in excel_df.iterrows():
+        print(index, row['NOMBRE'])
+
+if __name__ == '__main__':
+    main()
