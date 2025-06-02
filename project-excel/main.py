2e7b4d58c22571c8bd4839a58bdaf6c47965eca9
diegofdg <diegofdg@gmail.com>
2025-06-02 19:33:21 -0300

    ordenar alfabéticamente

--- project-excel/main.py
+++ project-excel/main.py
@@ -3,6 +3,21 @@ PATH_CARPETA = r'D:\mis_proyectos\ciencia_de_datos\automatizacion_de_reportes_co
 
 NOTAS_ALUMNOS_PATH = PATH_CARPETA + r'\Notas_Alumnos.xlsx'
 
+dict_asig = {
+    'LENGUA CASTELLANA Y LITERATURA':   'Lengua Castellana y Literatura',
+    'BIOLOGIA':                         'Biología',
+    'GEOGRAFIA E HISTORIA':             'Geografía e Historia',
+    'MATEMATICAS':                      'Matemáticas',
+    'INGLES':                           'Inglés',
+    'EDUCACION FISICA':                 'Educación Física',
+    'ETICA':                            'Ética',
+    'CULTURA CLASICA':                  'Cultura clásica',
+    'MUSICA':                           'Música',
+    'TECNOLOGIA':                       'Tecnología',
+    'EDUCACION PLASTICA':               'Educación Plástica',
+    'FRANCES':                          'Francés',
+}
+
 def main():
     excel_df = pd.read_excel(NOTAS_ALUMNOS_PATH, sheet_name='Notas')
     for index, row in excel_df.iterrows():
@@ -10,6 +25,12 @@ def main():
         
     asig_list = sorted(list(excel_df['ASIGNATURA'].drop_duplicates()))
     print(asig_list)
+    
+    filter_td_asig = []
+    for item in asig_list:
+        valorTd = dict_asig[item]
+        filter_td_asig.append(valorTd)
+    print('')
     
 if __name__ == '__main__':
     main()
