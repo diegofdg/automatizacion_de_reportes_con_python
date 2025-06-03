58966d376ae1c1101b50c903d14bcbde4a7808a6
diegofdg <diegofdg@gmail.com>
2025-06-03 00:20:19 -0300

    información al usuario del error

--- project-excel/main.py
+++ project-excel/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import sys
+
 PATH_CARPETA = r'D:\mis_proyectos\ciencia_de_datos\automatizacion_de_reportes_con_python\project-excel\inputs'
 
 NOTAS_ALUMNOS_PATH = PATH_CARPETA + r'\Notas_Alumnos.xlsx'
@@ -19,6 +21,7 @@ dict_asig = {
 }
 
 def deteccionErrores(df):
+    err1, err2, err3 = False, False, False
     alumnos_list = sorted(list(df['NOMBRE'].drop_duplicates()))
     asignatura_list = sorted(list(df['ASIGNATURA'].drop_duplicates()))
     
@@ -28,14 +31,25 @@ def deteccionErrores(df):
             #print(filt_al_as_df)
             if(len(filt_al_as_df) == 0):
                 print(f'Error: El alumno {al} no tiene la asignatura {asig} asignada')
+                err1 = True
             elif(len(filt_al_as_df) >1):
                 print(f'Error: El alumno {al} tiene la asignatura {asig} repetida {len(filt_al_as_df)} veces')
+                err2 = True
         
         for index, row in df.iterrows():
             trimestre_list = ['NOTA T1', 'NOTA T2', 'NOTA T3']
             for trim in trimestre_list:
                 if not ((row[trim] >= 0.0) and (row[trim] <=10.0)):
                     print(f'Error: El alumno {al} tiene el campo {trim} de la asignatura {asig} fuera de rango {str(row[trim])}')
+                    err3 = True
+        
+        if (err1 == True ) or (err2 == True) or (err3 == True):
+            print('')
+            print('Debes corregir los errores para continuar con la ejecucion del programa')
+            sys.exit(1)
+        else:
+            print('Ningun error detectado ')
+
 
 def main():
     excel_df = pd.read_excel(NOTAS_ALUMNOS_PATH, sheet_name='Notas')
