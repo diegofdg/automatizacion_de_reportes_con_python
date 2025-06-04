cfeeb305cd6da3baa720f8e9d6dd9c58d122f5a6
diegofdg <diegofdg@gmail.com>
2025-06-04 00:39:04 -0300

    más limpio aún

--- project-excel/main.py
+++ project-excel/main.py
@@ -15,6 +15,21 @@ PLANTILLA_CURSOS_PATH = PATH_CARPETA + r'\inputs\Plantilla_Notas.docx'
 
 CURSO = '2021/2022'
 
+dict_asig = {
+    'LENGUA CASTELLANA Y LITERATURA':   'Lengua Castellana y Literatura',
+    'BIOLOGIA':                         'Biología',
+    'GEOGRAFIA E HISTORIA':             'Geografía e Historia',
+    'MATEMATICAS':                      'Matemáticas',
+    'INGLES':                           'Inglés',
+    'EDUCACION FISICA':                 'Educación Física',
+    'ETICA':                            'Ética',
+    'CULTURA CLASICA':                  'Cultura clásica',
+    'MUSICA':                           'Música',
+    'TECNOLOGIA':                       'Tecnología',
+    'EDUCACION PLASTICA':               'Educación Plástica',
+    'FRANCES':                          'Francés',
+}
+
 def deteccionErrores(df):
     err1, err2, err3 = False, False, False
     alumnos_list = sorted(list(df['NOMBRE'].drop_duplicates()))
@@ -66,6 +81,13 @@ def eliminarCrearCarpetas(path):
     os.mkdir(path)
 
 def crearWordAsignarTag(datos_alumnos, excel_df):
+    asig_list = sorted(list(excel_df['ASIGNATURA'].drop_duplicates()))
+        
+    filter_td_asig = []
+    for item in asig_list:
+        valorTd = dict_asig[item]
+        filter_td_asig.append(valorTd)
+    
     nombre_Alumno_list = sorted(list(datos_alumnos['NOMBRE']))
     
     for nombre_alumno in nombre_Alumno_list:
@@ -92,31 +114,6 @@ def crearWordAsignarTag(datos_alumnos, excel_df):
         
         # Guardamos el documento
         docs_tpl.save(PATH_OUTPUT + '\\' + titulo)
-    
-
-def aniadirTildesAsignaturas(excel_df):
-    dict_asig = {
-        'LENGUA CASTELLANA Y LITERATURA':   'Lengua Castellana y Literatura',
-        'BIOLOGIA':                         'Biología',
-        'GEOGRAFIA E HISTORIA':             'Geografía e Historia',
-        'MATEMATICAS':                      'Matemáticas',
-        'INGLES':                           'Inglés',
-        'EDUCACION FISICA':                 'Educación Física',
-        'ETICA':                            'Ética',
-        'CULTURA CLASICA':                  'Cultura clásica',
-        'MUSICA':                           'Música',
-        'TECNOLOGIA':                       'Tecnología',
-        'EDUCACION PLASTICA':               'Educación Plástica',
-        'FRANCES':                          'Francés',
-    }
-    
-    asig_list = sorted(list(excel_df['ASIGNATURA'].drop_duplicates()))
-        
-    filter_td_asig = []
-    for item in asig_list:
-        valorTd = dict_asig[item]
-        filter_td_asig.append(valorTd)
-    
 
 def main():    
     eliminarCrearCarpetas(PATH_OUTPUT)    
@@ -124,10 +121,7 @@ def main():
     # Leemos notas y datos alumnos 
     excel_df = pd.read_excel(NOTAS_ALUMNOS_PATH, sheet_name='Notas')
     datos_alumnos = pd.read_excel(NOTAS_ALUMNOS_PATH, sheet_name='Datos_Alumnos')
-
-    # Agregamos tildes a las asignaturas        
-    aniadirTildesAsignaturas(excel_df)
-    
+        
     # Detectamos errores
     deteccionErrores(excel_df)
     
