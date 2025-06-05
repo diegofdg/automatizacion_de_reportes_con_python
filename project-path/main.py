851b7db2c2036f360a1652af83e5a05d684392ff
diegofdg <diegofdg@gmail.com>
2025-06-05 18:41:06 -0300

    crear ficheros

--- project-path/main.py
+++ project-path/main.py
@@ -50,7 +50,7 @@ import calendar
         # print('No hay archivos')
         
 ### Cambiar extensión de un archivo
-folder = Path('project-path/extensiones')
+# folder = Path('project-path/extensiones')
 
 # Pasar de txt a csv
 # for path in list(folder.iterdir()):
@@ -59,6 +59,21 @@ folder = Path('project-path/extensiones')
         # path.rename(nuevoNombreExtension)
 
 # Pasar de csv a txt
-for path in folder.glob('**/*.csv'):    
-    nuevoNombreExtension = path.with_suffix('.txt')
-    path.rename(nuevoNombreExtension)
+# for path in folder.glob('**/*.csv'):    
+    # nuevoNombreExtension = path.with_suffix('.txt')
+    # path.rename(nuevoNombreExtension)
+
+### Crear ficheros
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for i in numeros:
+    # with open ('project-path/test/' + f'test{i}.txt', 'w') as file:
+        # file.write('Hola Mundo')
+
+### Eliminar todos los archivos .txt
+# for path in Path('project-path/test/').glob('*.txt'):
+    # path.unlink()
+
+### Eliminar todos los archivos .txt menos el test9
+for path in Path('project-path/test/').glob('test[1-8].txt'):
+    path.unlink()
+ 
