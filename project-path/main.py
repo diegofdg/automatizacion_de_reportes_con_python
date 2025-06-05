3656b179addee64e4f2302e43f814ea8d9985687
diegofdg <diegofdg@gmail.com>
2025-06-05 00:19:02 -0300

    cambiar nombre de carpetas y archivos

--- project-path/main.py
+++ project-path/main.py
@@ -1,15 +1,29 @@
 from pathlib import Path
 import calendar
 
-# Creamos una carpeta
-# PATH_CARPETA = r'D:\mis_proyectos\ciencia_de_datos\automatizacion_de_reportes_con_python\project-path'
-# Path(PATH_CARPETA + r'\nueva_carpeta').mkdir(exist_ok=True)
+### Creamos una carpeta
+# Path('project-path/nueva_carpeta').mkdir(exist_ok=True)
 
-# Path('carpeta1/carpeta2/carpeta3').mkdir(parents=True, exist_ok=True)
+### Creamos carpetas anidadas
+# Path('project-path/carpeta1/carpeta2/carpeta3').mkdir(parents=True, exist_ok=True)
 
-meses_anio = list(calendar.month_name[1:])
-dias_semana = ['Dia 1', 'Dia 10', 'Dia 20', 'Dia 30']
+### Creamos una carpeta por cada mes y por cada dia
+# meses_anio = list(calendar.month_name[1:])
+# dias_semana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
+# for i, mes in enumerate(meses_anio):
+    # for dia in dias_semana:
+        # Path(f'project-path/2022/{i+1}.{mes}/{dia}').mkdir(parents=True, exist_ok=True)
 
-for i, mes in enumerate(meses_anio):
-    for dia in dias_semana:
-        Path(f'2022/{i+1}.{mes}/{dia}').mkdir(parents=True, exist_ok=True)
+### Renombrando una carpeta
+# path = Path('project-path/test')
+# path.rename('project-path/nuevo_test')
+
+### Renombrar archivo dentro de una carpeta
+# path = Path('project-path/nuevo_test/gastos.txt')
+# nuevoNombreCarpeta = path.with_name('gastos_diciembre.txt')
+# path.rename(nuevoNombreCarpeta)
+
+### Obtener path de los subdirectorios inmediatos
+carpeta = Path('project-path/2022')
+for path in list(carpeta.iterdir()):
+    print(path)
