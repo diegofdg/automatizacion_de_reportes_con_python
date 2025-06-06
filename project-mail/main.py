3ac644357f270b48191fb78ecd43ab02dfa0ba9a
diegofdg <diegofdg@gmail.com>
2025-06-06 00:23:34 -0300

    enviar una imagen como adjunto

--- project-mail/main.py
+++ project-mail/main.py
@@ -1,6 +1,7 @@
 from email.message import EmailMessage
 import ssl
 import smtplib
+import imghdr
 
 email_emisor = 'xxxxxxxx@gmail.com'
 email_password = 'xxxx xxxx xxxx xxxx'
@@ -13,6 +14,12 @@ em['FROM'] = email_emisor
 em['to'] = email_receptor
 em['Subject'] = asunto
 em.set_content(cuerpo)
+
+with open('project-mail/image.png', 'rb') as file:
+    file_data = file.read()
+    file_tipo = imghdr.what(file.name)
+    file_nombre = file.name
+em.add_attachment(file_data, filename=file_nombre, subtype=file_tipo, maintype='image')
 
 context = ssl.create_default_context()
 
